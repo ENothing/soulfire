e1812dda088af7196c6fd3d5c52e88ab1ce48339
EricNothing <814583328@qq.com>
2019-11-07 19:13:21 +0800

    修改article模型

--- controllers/app/v1/bbs/list.go
+++ controllers/app/v1/bbs/list.go
@@ -1,10 +1,34 @@
 package bbs
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"soulfire/models"
+	"soulfire/pkg/rsp"
+	"strconv"
+)
 
 func ArticleList(ctx *gin.Context)  {
 
+	cateId,_ :=strconv.ParseInt(ctx.DefaultQuery("cate_id","0"),10,64)
+	title := ctx.Query("title")
+	sort,_ := strconv.ParseInt(ctx.DefaultQuery("sort","0"),10,64)
+	page,_ := strconv.ParseInt(ctx.DefaultQuery("page","1"),10,64)
+	pageSize,_ := strconv.ParseInt(ctx.DefaultQuery("pageSize","10"),10,64)
 
+	data := make(map[string]interface{})
+
+	activities,total,lastPage,err := models.ActivityPaginate(page,pageSize,sort,cateId,title)
+
+	if err != nil {
+		rsp.JsonResonse(ctx,rsp.ArticleNotExits,nil,"")
+		return
+	}
+
+	data["total"] = total
+	data["last_page"] = lastPage
+	data["activities"] = activities
+
+	rsp.JsonResonse(ctx,rsp.OK,data,"")
 
 
 
