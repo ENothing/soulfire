4ddb9b999bb9ce9c1d9c9bc65e4aad288f0525d1
ENothing <814583328@qq.com>
2020-01-19 01:03:03 +0800

    修改banner接口返回值

--- controllers/app/v1/activity/index.go
+++ controllers/app/v1/activity/index.go
@@ -1,6 +1,7 @@
 package activity
 
 import (
+	"github.com/jinzhu/gorm"
 	"soulfire/models"
 	"soulfire/pkg/rsp"
 	"github.com/gin-gonic/gin"
@@ -11,33 +12,33 @@ func Index(ctx *gin.Context) {
 	data := make(map[string]interface{})
 
 	activityBanners, err := models.GetBannersByCate(1)
-	if err != nil {
-
-		rsp.JsonResonse(ctx, rsp.BannersNotExits, nil,"")
-		return
-
+	if err != nil || err == gorm.ErrRecordNotFound {
+		data["activity_banners"] = ""
+	}else{
+		data["activity_banners"] = activityBanners
 	}
 
-	activityAnnounce, _ := models.GetBannerByCate(4)
+	activityAnnounce, err := models.GetBannerByCate(4)
+	if err != nil ||  err == gorm.ErrRecordNotFound {
+		data["activity_ann"] = ""
+	}else{
+		data["activity_ann"] = activityAnnounce
+	}
 
-	activityCates,err := models.GetActivityCateLimitNum(5)
-	if err != nil {
-		rsp.JsonResonse(ctx, rsp.ActivityCateNotExits, nil,"")
-		return
+	activityCates,err := models.GetActivityCateLimitNum(6)
+	if err != nil || err == gorm.ErrRecordNotFound {
+		data["activity_cates"] = ""
+	}else{
+		data["activity_cates"] = activityCates
 	}
 
 	activityVideo, err := models.GetBannerByCate(3)
-	if err != nil {
-
-		rsp.JsonResonse(ctx, rsp.VideoNotExits, nil,"")
-		return
-
+	if err != nil || err == gorm.ErrRecordNotFound {
+		data["activity_video"] = ""
+	}else{
+		data["activity_video"] = activityVideo
 	}
 
-	data["activity_banners"] = activityBanners
-	data["activity_ann"] = activityAnnounce
-	data["activity_cates"] = activityCates
-	data["activity_video"] = activityVideo
 
 	rsp.JsonResonse(ctx, rsp.OK, data,"")
 
--- models/banner.go
+++ models/banner.go
@@ -10,6 +10,8 @@ type Banner struct {
 	CateId string `json:"cate_id" gorm:"column:cate_id;not null" binding:"required"`
 	Thumb string `json:"thumb" gorm:"column:thumb;not null" binding:"required" `
 	Url string `json:"url" gorm:"column:url"`
+	VideoUrl string `json:"video_url" gorm:"column:video_url"`
+	ShowType int64 `json:"show_type" gorm:"column:show_type"`
 	CreatedAt time.Time `gorm:";column:created_at" json:"created_at"`
 	UpdatedAt time.Time `gorm:";column:updated_at" json:"updated_at"`
 	DeletedAt *time.Time `gorm:"column:deleted_at" sql:"index" json:"deleted_at"`
