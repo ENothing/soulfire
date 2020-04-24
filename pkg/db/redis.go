95a7d38e560cddb485970cc57a884861c6c324bc
EricNothing <814583328@qq.com>
2020-04-24 17:38:06 +0800

    redis

--- pkg/db/redis.go
+++ pkg/db/redis.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"github.com/go-redis/redis/v7"
-	"time"
 )
 
 var RedisDb *redis.Client
@@ -13,9 +12,9 @@ func RedisInit()  {
 		Addr:     "127.0.0.1:6379", // Redis地址
 		Password: "GUhcF7bSK?u@Rtp",  // Redis账号
 		DB:       1,   // Redis库
-		PoolSize: 10,  // Redis连接池大小
-		MaxRetries: 3,              // 最大重试次数
-		IdleTimeout: 10*time.Second,            // 空闲链接超时时间
+		//PoolSize: 10,  // Redis连接池大小
+		//MaxRetries: 3,              // 最大重试次数
+		//IdleTimeout: 10*time.Second,            // 空闲链接超时时间
 	})
 	_, err := RedisDb.Ping().Result()
 	if err != nil {
