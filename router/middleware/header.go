04726bdab1e383f393548912a27495f4dae48fe9
EricNothing <814583328@qq.com>
2020-04-26 10:39:12 +0800

    header

--- router/middleware/header.go
+++ router/middleware/header.go
@@ -10,16 +10,6 @@ import (
 */
 func Options(c *gin.Context) {
 
-	//if c.Request.Method != "OPTIONS" {
-	//	c.Next()
-	//}
-	//
-	//c.Header("Access-Control-Allow-Origin", "*")
-	//c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
-	//c.Header("Access-Control-Allow-Headers", "authorization, origin, content-type, accept")
-	////c.Header("Allow", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
-	////c.Header("Content-Type", "application/json")
-	//c.AbortWithStatus(200)
 	method := c.Request.Method
 
 	c.Header("Access-Control-Allow-Origin", "*")
@@ -28,12 +18,10 @@ func Options(c *gin.Context) {
 	c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 	c.Header("Access-Control-Allow-Credentials", "true")
 
-	//放行所有OPTIONS方法
 	if method == "OPTIONS" {
 		c.AbortWithStatus(http.StatusNoContent)
 	}
-	// 处理请求
-	c.Next()
 
+	c.Next()
 
 }
