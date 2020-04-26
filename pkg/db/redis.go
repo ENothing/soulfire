b9c5413c8d75558310d30a3d78a51a23d78bab7a
EricNothing <814583328@qq.com>
2020-04-26 16:39:47 +0800

    redis连接超时设置

--- pkg/db/redis.go
+++ pkg/db/redis.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gomodule/redigo/redis"
 	"github.com/spf13/viper"
 	"soulfire/pkg/logging"
+	"time"
 )
 
 var R redis.Conn
@@ -13,8 +14,9 @@ func RedisInit()  {
 
 	var err error
 	address := viper.GetString("Redis.address")+":"+viper.GetString("Redis.port")
-	R ,err = redis.Dial("tcp",address)
 
+	R ,err = redis.Dial("tcp",address)
+	redis.DialConnectTimeout(60*time.Second)
 	if err != nil {
 		fmt.Println(err.Error())
 		logging.Logging(logging.ERR,err.Error())
